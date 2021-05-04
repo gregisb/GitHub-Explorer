dc982f315d87ffa562670e85a44d63edb4174243
GuilhermeUser <guilhermeb.regis@gmail.com>
2021-05-04 19:54:53 -0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
   filename: 'bundle.js'
 },
+devServer: {
+  contentBase: path.resolve(__dirname, 'public')
+},
+
+plugins: [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'public', 'index.html')
+  })
+],
+
 
 resolve: {
   extensions: ['.js', '.jsx']
@@ -18,6 +32,11 @@ module: {
       test: /\.jsx$/,
       exclude: /node_modules/,
       use: 'babel-loader'
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: ['style-loader', 'css-loader']
     }
   ]
 }
