1b8c972603698e07b07a6706aa5dc359027e0531
GuilhermeUser <guilhermeb.regis@gmail.com>
2021-05-05 20:49:09 -0300

    quarta

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
 export function App() {
-  return <h1>Hello Wolrd</h1>
+  return (
+    <>
+  <RepositoryList />
+  <Counter />
+  </>
+  )
 }
